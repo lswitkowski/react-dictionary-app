c2e2b3502bc3d45c49b144f987cf29e9241c13cb
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-11-16 14:56:26 -0800

    Add photo API, render photos, alt text, links

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,31 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
+
 
 export default function Dictionary(props){
 let [keyword, setKeyword] = useState(props.defaultKeyword);
-let [results, setResults]= useState("");
+let [results, setResults]= useState(null);
+let [photos, setPhotos] = useState(null);
 let [loaded, setLoaded]= useState(false);
+
 
 function handleResponse(response) {
   setResults(response.data);
 }
 
-    function search(event) {
+function handleResponsePics(response) {
+  setPhotos(response.data.photos);
+}
+    function search() {
       let apiKey = "65ae2e8ao4f01409ca53644a9atfcbed";
       let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
       axios.get(apiUrl).then(handleResponse);
+
+       
+       let apiUrlPics = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+       axios.get(apiUrlPics).then(handleResponsePics);
     }
 
 
@@ -41,18 +52,20 @@ if(loaded){
       <div className="dictionary">
         <h1 className="mt-5">Dictionary</h1>
         <div className="searchbox">
-        <h5 className="mt-1">Give me a word!</h5>
-        <form className="mt-2" onSubmit={handleSubmit}>
-          <input
-            type="search"
-            className="searchbar"
-            required
-            onChange={handleKeywordChange}
-          />
-          <input type="submit" className="submit" value="&#128270;" />
-        </form></div>
-       
+          <h5 className="mt-1">Give me a word!</h5>
+          <form className="mt-2" onSubmit={handleSubmit}>
+            <input
+              type="search"
+              className="searchbar"
+              required
+              onChange={handleKeywordChange}
+            />
+            <input type="submit" className="submit" value="&#128270;" />
+          </form>
+        </div>
+
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   
---
+++ src/Photos.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+export default function Photos(props) {
+  if (props.photos) {
+    console.log(props.photos);
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.photographer_url} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.alt}
+                  />
+                </a>
+              </div>
+            );
+            
+          })}
+        </div>
+      </section>
+    
+  );
+  } else {
+    return null;
+  }
+}
